66a389600ceec807dbb150e410dda67ec2c52379
Colin Wang <chenyangw233@gmail.com>
2020-09-23 13:40:23 +1000

    add email field to user table

--- web/src/pages/login.tsx
+++ web/src/pages/login.tsx
@@ -15,7 +15,7 @@ export const Login: React.FC<{}> = ({}) => {
   return (
     <Wrapper variant="small">
       <Formik
-        initialValues={{ username: '', password: '' }}
+        initialValues={{ usernameOrEmail: '', password: '' }}
         onSubmit={async (values, { setErrors }) => {
           const response = await login(values);
           if (response.data?.login.errors) {
@@ -30,9 +30,9 @@ export const Login: React.FC<{}> = ({}) => {
           <Form>
             <FormControl>
               <InputField
-                name="username"
-                placeholder="username"
-                label="Username"
+                name="usernameOrEmail"
+                placeholder="username or email"
+                label="Username or Email"
               />
               <Box mt={4}>
                 <InputField
